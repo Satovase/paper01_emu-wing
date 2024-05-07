f411f2c4b1db26f71b102c065a326eebaf7aece2
Satovase <105902101+Satovase@users.noreply.github.com>
2024-05-07 19:58:31 +0900

    Update Cellranger.sh

--- Cellranger.sh
+++ Cellranger.sh
@@ -1,8 +1,16 @@
 # cellranger-7.1.0
-cellranger mkgtf genomic.gtf droNov_refseq.filtered.gtf --attribute=gene_biotype:protein_cording
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/342/905/GCF_003342905.1_droNov1
+cellranger mkgtf GCF_003342905.1_droNov1_genomic.gtf droNov_refseq.filtered.gtf --attribute=gene_biotype:protein_cording
 cellranger mkref --genome=droNov_refseq_genome --fasta=GCF_003342905.1_droNov1_genomic.fna --genes=droNov_refseq.filtered.gtf
+
+# emu stage 25
+cellranger count --id=run_count_EmuHH25sc_refseq \
+   --fastqs=EmuHH25-ScRNA_fastqs \
+   --sample=EmuHH25-ScRNA \
+   --transcriptome=droNov_refseq_genome
+
+# emu stage 20-21
 cellranger count --id=run_count_EmuHH20-21sc_refseq \
    --fastqs=EmuHH20-21ScRNA_fastqs \
    --sample=EmuHH2021scRNA \
-   --transcriptome=droNov_refseq_genome >output.txt
-
+   --transcriptome=droNov_refseq_genome
